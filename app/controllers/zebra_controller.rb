fd55a947eb63eee49fb69d7b3fe4eef31b659e93
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-04-17 00:20:45 +0000

    first commit

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,9 @@
+class ZebraController < ApplicationController
+  def giraffe
+    @random_move = ["rock", "paper", "scissors"].sample
+
+
+
+    render({:template=> "game_templates/play_rock"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+get("/rock", { :controller=>"zebra", :action=>"giraffe"})
+
+
+
+
 end
